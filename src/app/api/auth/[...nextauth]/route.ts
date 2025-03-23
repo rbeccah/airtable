ef213a7eb77faee505f00f0cdeb7a9c33ede5df7
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-24 00:03:30 +1100

    Fix types to deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
-import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import { authOptions } from "~/lib/auth"
 
-const handler: NextApiHandler = NextAuth(authOptions);
+const handler: Request = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
