094a3857e2c49231063f8bae7339bf47777407a3
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-24 01:41:40 +1100

    Modify eslint rule

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/lib/auth";
 
-const handler: Request = NextAuth(authOptions) as Request;
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
