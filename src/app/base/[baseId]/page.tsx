ebed7eb7cb00b25b4840a5441d9c87005b2f74c6
Rebecca Hsu <rbeccahsu@gmail.com>
2025-03-24 02:12:53 +1100

    Update database for deployment

--- src/app/base/[baseId]/page.tsx
+++ src/app/base/[baseId]/page.tsx
@@ -67,6 +67,9 @@ const Base = () => {
         <div className="bg-gray-100 h-full">
           <AirTable tableData={selectedTableData} tableId={selectedTableId}/>
         </div>
+        <div>
+          hello
+        </div>
       </div>
     </SaveProvider>
   );
